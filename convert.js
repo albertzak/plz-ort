21d75ceef067019f0ba176b47ebf1131c819dd42
Albert Zak <me@albertzak.com>
2018-02-16 23:05:29 +0100

    Automate downloading and conversion

--- convert.js
+++ convert.js
@@ -1,70 +1,128 @@
-var fs = require('fs')
+const fs = require('fs')
+const XLSX = require('xlsx')
+const fetch = require('node-fetch')
 
-// Step 1: Download latest "PLZ Verzeichnis" from
-// https://www.post.at/en/business_advertise_products_and_services_addresses_postcodes.php
+const main = async () => {
+  try {
+    const { filename, date } = await download()
+    const csv = await convert(filename)
+    await write(csv)
+    await publish(date)
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-// Step 2: Convert XLS to CSV
-// using eg. https://convertio.co/document-converter/
+const download = async () => {
+  return new Promise(async (resolve, reject) => {
+    const page = await fetch('https://www.post.at/geschaeftlich_werben_produkte_und_services_adressen_postlexikon.php')
+    const html = await page.text()
+    const { link, filename, date } = extractLink(html)
+    const xlsFile = await fetch('https://www.post.at/' + link)
 
-// Step 3: Edit filename below, run with
-//   $ node convert.js
-// and re-publish with updated version number
-//   major.minor-YYYY.MM
+    const stream = fs.createWriteStream(filename)
+    await xlsFile.body.pipe(stream)
 
-var CSV = 'PLZ_Verzeichnis_JAN18.csv'
-var JS = 'data.js'
-
-var parseLine = function (line) {
-  var fields = line
-    .split('","')
-    .slice(0, 2)
-    .map(function (cell) {
-      if (cell[0] === '"') {
-        return cell.slice(1, cell.length)
-      } else {
-        return cell
-      }
+    stream.on('finish', () => {
+      console.log('Downloaded', filename)
+      resolve({ filename, date })
+    }).on('error', (e) => {
+      console.error(e)
+      reject(e)
     })
-
-  return {
-    plz: fields[0],
-    ort: fields[1]
-  }
-}
-
-var collect = function (acc, parsed) {
-  if (parsed.plz && parsed.ort) {
-    acc[parsed.plz] = parsed.ort
-  }
-  return acc
-}
-
-fs.readFile(CSV, 'utf-8', function (err, content) {
-  if (err) {
-    console.error('Failed to read file', CSV, err)
-    return
-  }
-
-  var lines = content
-    .split('\n')
-    .slice(1)
-
-  var data = lines
-    .map(parseLine)
-    .reduce(collect, {})
-
-  console.log(data)
-
-  var stringified = JSON.stringify(data, null, 2)
-  var output = '// auto-generated file\n' +
-    'module.exports = ' + stringified
-
-  fs.writeFile(JS, output, function (err) {
-    if (err) {
-      console.error('Failed to write file', JS, err)
-      return
-    }
-
-    console.log('Done, saved', JS)
   })
-})
+}
+
+const extractLink = html => {
+  const linkRegex = /(\/downloads\/(PLZ_Verzeichnis_(...\d\d)\.xls)\?\d+)/
+
+  const match = html.match(linkRegex)
+  if (!match) {
+    console.log(html)
+    throw new Error('Cannot find link to PLZ_Verzeichnis_XXXYY.xls')
+  }
+
+  const link = match[1]
+  const filename = match[2]
+  const date = parseDate(match[3])
+
+  console.log('Extracted', { link, filename, date })
+  return { link, filename, date }
+}
+
+const parseDate = s => {
+  // input format: FEB18
+  const month = {
+    JAN: 1,
+    FEB: 2,
+    MAR: 3,
+    APR: 4,
+    MAI: 5,
+    MAY: 5,
+    JUN: 6,
+    JUL: 7,
+    AUG: 8,
+    SEP: 9,
+    OKT: 10,
+    NOV: 11,
+    DEZ: 12,
+    DEC: 12
+  }[s.slice(0, 3)]
+  const year = '20' + s.slice(3)
+
+  console.log('Parsed date', { month, year })
+
+  return [year, month].join('.')
+}
+
+const convert = xlsFile => {
+  const buf = fs.readFileSync(xlsFile)
+  console.log('File size', buf.length)
+  const workbook = XLSX.read(buf)
+
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+
+  const json = XLSX.utils.sheet_to_json(worksheet)
+
+  const result = json.reduce((acc, parsed) => {
+    if (parsed.PLZ && parsed.PLZ.length === 4 && parsed.Ort) {
+      acc[parsed.PLZ] = parsed.Ort
+    }
+    return acc
+  }, {})
+
+  console.log('Parsed', Object.keys(result).length, 'entries')
+
+  if (Object.keys(result) < 2500) {
+    throw new Error('Should have more than 2500 entries')
+  }
+
+  return result
+}
+
+const write = data => {
+  const jsFile = 'data.js'
+
+  return new Promise((resolve, reject) => {
+    const stringified = JSON.stringify(data, null, 2)
+    const output = '// auto-generated file\n' +
+      'module.exports = ' + stringified
+
+    fs.writeFile(jsFile, output, err => {
+      if (err) {
+        console.error('Failed to write file', jsFile, err)
+        reject()
+        return
+      }
+
+      console.log('Done, saved', jsFile)
+      resolve(jsFile)
+    })
+  })
+}
+
+const publish = version => {
+  console.log('Published to npm as', version)
+}
+
+main()
